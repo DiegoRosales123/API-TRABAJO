987ee4fc92e5956e45fa3195b2ee39ac3095e796
DiegoRosales123 <di.rosales@duocuc.cl>
2023-05-08 22:00:12 -0400

    Añadi estilo a la Api de  Rick y Morty. Ahora se ve más bonita.

--- js/rickandmortyapi.js
+++ js/rickandmortyapi.js
@@ -1,44 +1,53 @@
 function getCharacters(done) {
 
-const results = fetch("https://rickandmortyapi.com/api/character");
-
-
-results
-    .then(response => response.json())
-    .then(data => {
-            done(data)
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    
+    
+    results
+        .then(response => response.json())
+        .then(data => {
+                done(data)
+        });
+    
+    
+    }
+    
+    getCharacters(data => {
+    
+        data.results.forEach(personaje => {
+    
+            const article = document.createRange().createContextualFragment(/*html*/`
+            
+            <article>
+           
+            <section class="card">
+              <div class="product-image">
+                <img src="${personaje.image}" alt="Imagen de Personajes" />
+              </div>
+              <div class="product-info">
+                <h2>${personaje.name}</h2>
+                <p>Genero: ${personaje.gender} - Especie: ${personaje.species}  </p>
+                <div class="localizacion">Localizacion: ${personaje.location.name}</div>
+              </div>
+              <div class="btn">
+                <button class="fav">
+                  <svg class="svg" id="i-star" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                    <path d="M16 2 L20 12 30 12 22 19 25 30 16 23 7 30 10 19 2 12 12 12 Z" />
+                  </svg>
+                </button>
+              </div>
+            </section>
+    
+    
+             </article> 
+             
+             `
+            );
+    
+            const main = document.querySelector("main");
+    
+            main.append(article);
+        });
+    
+    
     });
-
-
-}
-
-getCharacters(data => {
-
-    data.results.forEach(personaje => {
-
-        const article = document.createRange().createContextualFragment(/*html*/`
-        
-        <article>
-
-        <div class="image-container">
-            <img src="${personaje.image}" alt="Personaje">
-        </div>
-    
-    
-        <h2>${personaje.name}</h2>
-        <h3>${personaje.gender}</h3>
-        <span>${personaje.status}</span>
-
-    
-         </article> 
-         
-         `
-        );
-
-        const main = document.querySelector("main");
-
-        main.append(article);
-    });
-
-
-});
